d15fc314da3f2eb5638fb57601ac54712cc4d139
taramonaghan <taramonaghan1022@gmail.com>
2021-05-01 15:27:03 -0400

    update script.js

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,15 @@
+let pokemonList = [{
+    name: "Bulbasaur", 
+    height float: 0.7, 
+    type: ['grass', 'poison']
+  },
+  {
+    name: "Ivysaur",
+    height: 1,
+    type: ['grass', 'poison']
+  },
+  {
+    name: "Vensaur",
+    height: 2,
+    type: ['grass', 'poison']
+  }]
